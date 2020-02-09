57ff10827c8192cf3aee4cdd4f7f5b3b643226ae
Alex Edwards <lensgeek92@gmail.com>
2020-02-09 23:18:22 +0000

    Init Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,30 +1,46 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: '{Meta-Title}',
+    description: '{Meta-Desc}',
+    author: 'Alex Edwards @lexedwards',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: '{Manifest-Name}',
+        short_name: '{Name}',
+        start_url: '/',
+        background_color: '#000',
+        theme_color: '#000',
+        display: 'minimal-ui',
+        // icon : '' This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        implementation: require('sass'),
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-postcss',
+      options: {
+        postCssPlugins: [
+          require('postcss-preset-env')(),
+          require('autoprefixer')(),
+          require('cssnano')(),
+        ],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
