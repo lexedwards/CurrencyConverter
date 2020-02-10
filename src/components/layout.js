b0e38ce5126840949f43037b598027eb52edef23
Alex Edwards <lensgeek92@gmail.com>
2020-02-10 23:42:23 +0000

    Played with Gatsby-Image API

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,15 +8,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 import Header from './header'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
+    query {
       site {
         siteMetadata {
           title
+        }
+      }
+      profileImage: file(relativePath: { eq: "Social-1080.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
         }
       }
     }
@@ -33,9 +41,22 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with{' '}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        <footer className="md:flex md:mx-auto md:max-w-sm border-gray-600 bxs-natural bg-white rounded-lg p-6">
+          <Img
+            className="h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6"
+            fluid={data.profileImage.childImageSharp.fluid}
+          />
+          <div className="text-center md:text-left">
+            <p className="text-lg">Made by Alex Edwards</p>
+            <p className=" text-teal-600 ">Front-End Developer</p>
+            <a
+              href="//alexedwards.co"
+              rel="dns-prefetch noopener noreferrer"
+              target="_blank"
+            >
+              Look me up!
+            </a>
+          </div>
         </footer>
       </div>
     </>
