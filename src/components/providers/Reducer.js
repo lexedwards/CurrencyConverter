ed8dfcef7ae344e53e5b26a657c50d9a9ae6f5ff
Alex Edwards <lensgeek92@gmail.com>
2020-03-12 12:01:24 +0000

    Fixed bad Math

--- src/components/providers/Reducer.js
+++ src/components/providers/Reducer.js
@@ -1,19 +1,34 @@
 export default (state, { type, payload }) => {
   switch (type) {
     case 'onSelect':
-      const newState = { ...state };
+      let newState = { ...state };
       newState.convert[payload.Xrole] = payload.currency;
       newState.rate = newConvertRate(newState.convert, newState.rates);
       return newState;
     case 'updateRates':
       return { ...state, rates: payload.values };
+    case 'swapRates':
+      return {
+        ...state,
+        rate: newConvertRate(payload, state.rates),
+        convert: { ...payload },
+      };
     default:
       return state;
   }
 };
 
 const newConvertRate = ({ base, target }, rates) => {
-  const rounded = Math.round(rates[base] * rates[target] * 100);
-  const newRate = rounded / 100;
-  return newRate;
+  const baseRate = rates[base];
+  const targetRate = rates[target];
+
+  let rounded;
+
+  if (typeof baseRate === 'number' && typeof targetRate === 'number') {
+    rounded = (targetRate / baseRate) * 100;
+  } else {
+    rounded = "Error: Can't find Currency Exchange";
+  }
+
+  return rounded;
 };
