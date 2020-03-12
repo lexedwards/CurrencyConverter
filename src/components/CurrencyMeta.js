2cc62d28f4dc89c1a45dd7068a6990d547f102fc
Alex Edwards <lensgeek92@gmail.com>
2020-03-12 12:01:40 +0000

    Added Swap function

--- src/components/CurrencyMeta.js
+++ src/components/CurrencyMeta.js
@@ -3,16 +3,28 @@ import PropTypes from 'prop-types';
 import { useStoreValue } from './providers/Context';
 
 const CurrencyMeta = ({}) => {
-  let [{ rate }] = useStoreValue();
+  let [{ convert, rate }, dispatch] = useStoreValue();
 
   return (
-    <div className="flex">
-      <div className="convertRate">
-        <div className="w-6 h-6 rounded-full bg-teal-600 text-white text-center">
-          =
-        </div>
-        <p>Exchange Rate: {rate.toString()}</p>
+    <div className="convertRate flex">
+      <div className="w-6 h-6 rounded-full bg-teal-600 text-white text-center">
+        =
       </div>
+      <p>
+        Exchange Rate:{' '}
+        {typeof rate === 'number' ? (Math.round(rate) / 100).toString() : rate}
+      </p>
+      <button
+        className="w-6 h-6 rounded-full bg-green-600 text-white text-center"
+        onClick={() => {
+          dispatch({
+            type: 'swapRates',
+            payload: { base: convert.target, target: convert.base },
+          });
+        }}
+      >
+        ⇆
+      </button>
     </div>
   );
 };
